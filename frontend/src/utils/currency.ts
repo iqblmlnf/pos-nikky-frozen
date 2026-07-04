c3a2860a54f9aee683963850dcb4c7d7f9c82079
iqblmlnf <iqbalmaulanafajar36@gmail.com>
2026-07-05 05:16:43 +0700

    update lib dan utils

--- frontend/src/utils/currency.ts
+++ frontend/src/utils/currency.ts
@@ -1,19 +1,8 @@
 export const fmt = (
   value: number
 ): string => {
-<<<<<<< HEAD
-  return new Intl.NumberFormat(
-    "id-ID",
-    {
-      style: "currency",
-      currency: "IDR",
-      maximumFractionDigits: 0,
-    }
-  ).format(value);
-=======
   return (
     "Rp. " +
     Number(value).toLocaleString("id-ID")
   );
->>>>>>> 71d1392 (add finance feature)
 };
